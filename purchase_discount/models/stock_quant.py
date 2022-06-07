4865e7089dd206678a572ad5cae4359dc4cdf030
AhmedNabilTopaz <60816134+AhmedNabilTopaz@users.noreply.github.com>
2022-06-07 18:20:55 +0200

    Update stock_quant.py

--- purchase_discount/models/stock_quant.py
+++ purchase_discount/models/stock_quant.py
@@ -5,16 +5,17 @@ class StockQuants(models.Model):
     
     @api.model
     def _unlink_zero_quants(self):
-        """ _update_available_quantity may leave quants with no
-        quantity and no reserved_quantity. It used to directly unlink
-        these zero quants but this proved to hurt the performance as
-        this method is often called in batch and each unlink invalidate
-        the cache. We defer the calls to unlink in this method.
-        """
-        precision_digits = max(6, self.sudo().env.ref('product.decimal_product_uom').digits * 2)
-        # Use a select instead of ORM search for UoM robustness.
-        query = """SELECT id FROM stock_quant WHERE (round(quantity::numeric, %s) = 0 OR quantity IS NULL) AND round(reserved_quantity::numeric, %s) = 0;"""
-        params = (precision_digits, precision_digits)
-        self.env.cr.execute(query, params)
-        quant_ids = self.env['stock.quant'].browse([quant['id'] for quant in self.env.cr.dictfetchall()])
-        #quant_ids.sudo().unlink()
+        pass
+#         """ _update_available_quantity may leave quants with no
+#         quantity and no reserved_quantity. It used to directly unlink
+#         these zero quants but this proved to hurt the performance as
+#         this method is often called in batch and each unlink invalidate
+#         the cache. We defer the calls to unlink in this method.
+#         """
+#         precision_digits = max(6, self.sudo().env.ref('product.decimal_product_uom').digits * 2)
+#         # Use a select instead of ORM search for UoM robustness.
+#         query = """SELECT id FROM stock_quant WHERE (round(quantity::numeric, %s) = 0 OR quantity IS NULL) AND round(reserved_quantity::numeric, %s) = 0;"""
+#         params = (precision_digits, precision_digits)
+#         self.env.cr.execute(query, params)
+#         quant_ids = self.env['stock.quant'].browse([quant['id'] for quant in self.env.cr.dictfetchall()])
+#         #quant_ids.sudo().unlink()
